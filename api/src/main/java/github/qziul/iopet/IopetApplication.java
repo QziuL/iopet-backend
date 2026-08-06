034dd96592a6e9c89b15b2df5d1fad2d111466f1
QziuL <lf.quinholi2@gmail.com>
2026-08-06 13:55:28 -0300

    feat: config iniciais

---
+++ api/src/main/java/github/qziul/iopet/IopetApplication.java
@@ -0,0 +1,13 @@
+package github.qziul.iopet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IopetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IopetApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/github/qziul/iopet/IopetApplicationTests.java
@@ -0,0 +1,13 @@
+package github.qziul.iopet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IopetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
