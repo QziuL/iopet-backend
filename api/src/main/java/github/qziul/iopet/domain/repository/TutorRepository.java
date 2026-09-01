794d69924dccdcc883b426d696a2ba04302ded65
QziuL <lf.quinholi2@gmail.com>
2026-09-01 19:02:59 -0300

    refactor: update ITutorService and TutorRepository to use UUID for identifiers

--- api/src/main/java/github/qziul/iopet/domain/repository/TutorRepository.java
+++ api/src/main/java/github/qziul/iopet/domain/repository/TutorRepository.java
@@ -4,8 +4,11 @@ import github.qziul.iopet.domain.model.Tutor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+import java.util.UUID;
+
 @Repository
 public interface TutorRepository extends JpaRepository<Tutor, Long> {
-    Tutor findByEmail(String email);
-    Tutor findByUuid(String uuid);
+    Optional<Tutor> findByEmail(String email);
+    Optional<Tutor> findByUuid(UUID uuid);
 }
--- api/src/main/java/github/qziul/iopet/service/ITutorService.java
+++ api/src/main/java/github/qziul/iopet/service/ITutorService.java
@@ -4,12 +4,13 @@ import github.qziul.iopet.domain.model.Tutor;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 public interface ITutorService {
-    List<Tutor> listar();
-    Optional<Tutor> encontrarPorUuid(String uuid);
+    //List<Tutor> listar();
+    Optional<Tutor> encontrarPorUuid(UUID uuid);
     Optional<Tutor> encontrarPorEmail(String email);
     Tutor cadastrar(Tutor tutor); // To-Do TutorDTO
     Tutor atualizar(Tutor tutor);
-    boolean deletar(Long id);
+    void excluirConta(UUID uuid);
 }
