74d06b8fcdadb19100e96a15647b3a3e5823a80b
QziuL <lf.quinholi2@gmail.com>
2026-09-11 20:57:12 -0300

    feat: update DispositivoIotRepository to use String for ID and add findByEnderecoMac method

--- api/src/main/java/github/qziul/iopet/domain/repository/DispositivoIotRepository.java
+++ api/src/main/java/github/qziul/iopet/domain/repository/DispositivoIotRepository.java
@@ -4,6 +4,9 @@ import github.qziul.iopet.domain.model.DispositivoIot;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface DispositivoIotRepository extends JpaRepository<DispositivoIot, Long> {
+public interface DispositivoIotRepository extends JpaRepository<DispositivoIot, String> {
+    Optional<DispositivoIot> findByEnderecoMac(String enderecoMac);
 }
