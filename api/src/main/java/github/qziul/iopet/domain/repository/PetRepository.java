b92f66efa2ea082e6f77a5eef15a575fd0d6d92e
QziuL <lf.quinholi2@gmail.com>
2026-09-15 18:08:29 -0300

    feat: add EntityGraph annotations to PetRepository methods for eager loading

--- api/src/main/java/github/qziul/iopet/domain/repository/PetRepository.java
+++ api/src/main/java/github/qziul/iopet/domain/repository/PetRepository.java
@@ -1,6 +1,7 @@
 package github.qziul.iopet.domain.repository;
 
 import github.qziul.iopet.domain.model.Pet;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,7 +9,10 @@ import java.util.*;
 
 @Repository
 public interface PetRepository extends JpaRepository<Pet, Long> {
+    @EntityGraph(attributePaths = {"tutor"})
     Optional<Pet> findByUuid(UUID uuid);
+    @EntityGraph(attributePaths = {"tutor"})
     Optional<Pet> findByNome(String nome);
+    @EntityGraph(attributePaths = {"tutor"})
     List<Pet> findByTutorId(Long tutorId);
 }
