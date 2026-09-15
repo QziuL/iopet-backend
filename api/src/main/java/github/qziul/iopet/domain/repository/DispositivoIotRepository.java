615552b22662705530b09f42be2003fccb354130
QziuL <lf.quinholi2@gmail.com>
2026-09-15 14:18:26 -0300

    feat: remove unused import

--- api/src/main/java/github/qziul/iopet/domain/repository/DispositivoIotRepository.java
+++ api/src/main/java/github/qziul/iopet/domain/repository/DispositivoIotRepository.java
@@ -4,7 +4,5 @@ import github.qziul.iopet.domain.model.DispositivoIot;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface DispositivoIotRepository extends JpaRepository<DispositivoIot, String> { }
