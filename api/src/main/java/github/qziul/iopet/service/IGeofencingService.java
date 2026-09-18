56ed6b902cbc1aac9f5aff578c2131c9917deca0
QziuL <lf.quinholi2@gmail.com>
2026-09-18 18:20:18 -0300

    feat: add telemetry processing method to IGeofencingService interface

--- api/src/main/java/github/qziul/iopet/service/IGeofencingService.java
+++ api/src/main/java/github/qziul/iopet/service/IGeofencingService.java
@@ -1,5 +1,6 @@
 package github.qziul.iopet.service;
 
+import github.qziul.iopet.controller.dto.request.TelemetriaRequestDTO;
 import github.qziul.iopet.domain.model.DispositivoIot;
 
 public interface IGeofencingService {
@@ -10,8 +11,14 @@ public interface IGeofencingService {
     void processarPosicao(DispositivoIot dispositivo, double latitude, double longitude);
 
     /**
+     * Processa o pacote de telemetria recebido do dispositivo IoT via mensageria (RabbitMQ/MQTT),
+     * atualiza os metadados do dispositivo (bateria, última comunicação), persiste o histórico
+     * e avalia violação do perímetro de segurança.
+     */
+    void processarTelemetria(TelemetriaRequestDTO telemetria);
+
+    /**
      * Avalia se um determinado ponto está dentro do polígono de segurança do Pet.
      */
     boolean estaDentroDaZonaDeSeguranca(DispositivoIot dispositivo, double latitude, double longitude);
-
 }
