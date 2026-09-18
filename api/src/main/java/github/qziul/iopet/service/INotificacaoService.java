33311b877a7a9d60ec25831d4c1d1fa025bafe44
QziuL <lf.quinholi2@gmail.com>
2026-09-18 18:20:09 -0300

    feat: add method for low battery alert notification in notification service

--- api/src/main/java/github/qziul/iopet/service/INotificacaoService.java
+++ api/src/main/java/github/qziul/iopet/service/INotificacaoService.java
@@ -1,11 +1,13 @@
 package github.qziul.iopet.service;
 
 import github.qziul.iopet.domain.model.AlertaGeofencing;
+import github.qziul.iopet.domain.model.DispositivoIot;
 
 import java.util.Map;
 
 public interface INotificacaoService {
     void enviarNotificacaoPush(AlertaGeofencing alerta);
+    void enviarAlertaBateriaBaixa(DispositivoIot dispositivo, int nivelBateria);
     void enviarPorTopico(String topico, String titulo, String corpo, Map<String, String> dados);
     void enviarPorToken(String token, String titulo, String corpo, Map<String, String> dados);
 }
