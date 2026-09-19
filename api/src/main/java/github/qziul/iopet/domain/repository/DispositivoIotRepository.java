0f3adc529fafd60450cbd6f2c6b571c877c93fa8
QziuL <lf.quinholi2@gmail.com>
2026-09-18 21:31:27 -0300

    feat: add method to retrieve DispositivoIot by pet tutor ID with entity graph

--- api/src/main/java/github/qziul/iopet/domain/repository/DispositivoIotRepository.java
+++ api/src/main/java/github/qziul/iopet/domain/repository/DispositivoIotRepository.java
@@ -1,8 +1,14 @@
 package github.qziul.iopet.domain.repository;
 
 import github.qziul.iopet.domain.model.DispositivoIot;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface DispositivoIotRepository extends JpaRepository<DispositivoIot, String> { }
+public interface DispositivoIotRepository extends JpaRepository<DispositivoIot, String> {
+    @EntityGraph(attributePaths = {"pet"})
+    List<DispositivoIot> findByPetTutorId(Long tutorId);
+}
