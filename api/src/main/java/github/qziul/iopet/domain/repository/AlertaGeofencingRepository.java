c76d772212a2634dd78141e2c43da80c9593489f
QziuL <lf.quinholi2@gmail.com>
2026-09-18 21:26:48 -0300

    feat: add methods to retrieve AlertaGeofencing by pet tutor ID and UUID with entity graph

--- api/src/main/java/github/qziul/iopet/domain/repository/AlertaGeofencingRepository.java
+++ api/src/main/java/github/qziul/iopet/domain/repository/AlertaGeofencingRepository.java
@@ -1,9 +1,18 @@
 package github.qziul.iopet.domain.repository;
 
 import github.qziul.iopet.domain.model.AlertaGeofencing;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.UUID;
+
 @Repository
 public interface AlertaGeofencingRepository extends JpaRepository<AlertaGeofencing, Long> {
+    @EntityGraph(attributePaths = {"pet", "historicoLocalizacao"})
+    List<AlertaGeofencing> findByPetTutorIdOrderByDataDesc(Long tutorId);
+
+    @EntityGraph(attributePaths = {"pet", "historicoLocalizacao"})
+    List<AlertaGeofencing> findByPetUuidOrderByDataDesc(UUID petUuid);
 }
