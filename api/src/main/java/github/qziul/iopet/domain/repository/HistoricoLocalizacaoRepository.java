91aae2697498c00dd0f8efd505ba0c4c0fe728ed
QziuL <lf.quinholi2@gmail.com>
2026-09-18 21:31:33 -0300

    feat: add methods to retrieve latest and paginated location history by DispositivoIot

--- api/src/main/java/github/qziul/iopet/domain/repository/HistoricoLocalizacaoRepository.java
+++ api/src/main/java/github/qziul/iopet/domain/repository/HistoricoLocalizacaoRepository.java
@@ -1,9 +1,16 @@
 package github.qziul.iopet.domain.repository;
 
+import github.qziul.iopet.domain.model.DispositivoIot;
 import github.qziul.iopet.domain.model.HistoricoLocalizacao;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface HistoricoLocalizacaoRepository extends JpaRepository<HistoricoLocalizacao, Long> {
+    Optional<HistoricoLocalizacao> findTop1ByDispositivoIotOrderByDataDesc(DispositivoIot dispositivoIot);
+    List<HistoricoLocalizacao> findByDispositivoIotOrderByDataDesc(DispositivoIot dispositivoIot, Pageable pageable);
 }
