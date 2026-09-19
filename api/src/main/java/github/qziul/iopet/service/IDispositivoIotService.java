fd339704a13c9232e7f9e9a27e2155d965290f6c
QziuL <lf.quinholi2@gmail.com>
2026-09-18 21:31:39 -0300

    feat: add method to list devices by tutor ID in IDispositivoIotService

--- api/src/main/java/github/qziul/iopet/service/IDispositivoIotService.java
+++ api/src/main/java/github/qziul/iopet/service/IDispositivoIotService.java
@@ -1,7 +1,11 @@
 package github.qziul.iopet.service;
 
+import github.qziul.iopet.controller.dto.response.DispositivoResponseDTO;
+
+import java.util.List;
 import java.util.UUID;
 
 public interface IDispositivoIotService {
     void vincularDispositivoAoPet(UUID idPublicoPet, String enderecoMac);
+    List<DispositivoResponseDTO> listarDispositivosDoTutor(Long tutorId);
 }
